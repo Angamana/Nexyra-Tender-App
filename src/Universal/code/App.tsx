48b8b0bba4856d45b81c61c9ecd43e3a3adb3112
Angamana <developer@nexyra.com>
2026-07-30 11:18:42 +0200

    Configure GitHub Pages deploy workflow & Profile auth system

--- src/Universal/code/App.tsx
+++ src/Universal/code/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './lib/auth'
 import { CompanyProvider } from './lib/companyContext'
 import { TenderProvider } from './lib/tenderContext'
